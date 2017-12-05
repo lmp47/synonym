d1b63944aaa0d0868689fe4020e84edba7a54fc4
Lauren Pick <lpick@cs.princeton.edu>
2017-12-05 17:21:56 -0500

    Make Lockstep comparable to Consolidation

--- descartes/src/Analysis/Lockstep.hs
+++ descartes/src/Analysis/Lockstep.hs
@@ -44,14 +44,13 @@ verifyLs opt classMap _comps prop = do
  (pre, post) <- trace ("after prelude:" ++ show (objSort, pars, res, fields)) $ prop (pars, res, fields)
  -- debug
  preStr  <- astToString pre
- let k = T.trace preStr $ unsafePerformIO $ getChar
  --
- (fields', axioms) <- k `seq` addAxioms objSort fields
+ (fields', axioms) <- addAxioms objSort fields
  let blocks = zip [0..] $ getBlocks comps
  iSSAMap <- getInitialSSAMap
 -- let iEnv = Env objSort pars res fields' iSSAMap M.empty axioms pre post post opt False False 0
  -- set debug and fuse
- let iEnv = Env objSort pars res fields' iSSAMap M.empty axioms pre post post opt True False 0
+ let iEnv = Env objSort pars res fields' iSSAMap M.empty axioms pre post post opt False False 0
  ((res, mmodel),_) <- runStateT (analyser (Composition blocks [] [])) iEnv
  case res of 
   Unsat -> return (Unsat, Nothing)
